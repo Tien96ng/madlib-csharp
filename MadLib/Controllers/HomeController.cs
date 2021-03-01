c809a25938823c003a2f08b25cfa087eecc8e3eb
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-01 11:55:28 -0800

    add proper routing and paths to link up pages
    Co-authored-by: Tien Nguyen <tien96ng@gmail.com>
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -7,46 +7,51 @@ namespace MadLib.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult MadLibs()
+    public ActionResult StorySelector()
     {
-      // PathVariable newPath = new PathVariable();
-      // newPath.PathSetter = "action=/" + path;
       return View();
+    }
+
+    [Route("/madlibsform")]
+    public ActionResult MadLibs(string path)
+    {
+      PathVariable newPath = new PathVariable();
+      newPath.PathSetter = "/" + path;
+      return View(newPath);
     }
 
     [Route("/zoo")]
     public ActionResult Zoo(string noun, string adjective, string verb, string place, string name)
     {
       MadLibVariables myMadLibVariables = new MadLibVariables();
+      Console.WriteLine(noun);
+      myMadLibVariables.Name = name;
+      myMadLibVariables.Verb = verb;
       myMadLibVariables.Noun = noun;
       myMadLibVariables.Adjective = adjective;
-      myMadLibVariables.Verb = verb;
       myMadLibVariables.Place = place;
-      myMadLibVariables.Name = name;
       return View(myMadLibVariables);
     }
 
     [Route("/goosebumps")]
-    public ActionResult Goosebumps(string noun, string adjective, string verb, string place, string name)
+    public ActionResult Goosebumps(string noun, string verb, string name)
     {
       MadLibVariables myMadLibVariables = new MadLibVariables();
-      myMadLibVariables.Noun = noun;
-      myMadLibVariables.Adjective = adjective;
-      myMadLibVariables.Verb = verb;
-      myMadLibVariables.Place = place;
       myMadLibVariables.Name = name;
+      myMadLibVariables.Verb = verb;
+      myMadLibVariables.Noun = noun;
       return View(myMadLibVariables);
     }
 
     [Route("/puppies")]
-    public ActionResult Puppies(string noun, string adjective, string verb, string place, string name)
+    public ActionResult Puppies(string noun, string noun2, string adjective, string adjective2, string adverb)
     {
       MadLibVariables myMadLibVariables = new MadLibVariables();
       myMadLibVariables.Noun = noun;
+      myMadLibVariables.Noun2 = noun2;
       myMadLibVariables.Adjective = adjective;
-      myMadLibVariables.Verb = verb;
-      myMadLibVariables.Place = place;
-      myMadLibVariables.Name = name;
+      myMadLibVariables.Adjective2 = adjective2;
+      myMadLibVariables.Adverb = adverb;
       return View(myMadLibVariables);
     }
   }
